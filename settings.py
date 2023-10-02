ebf8a97376f2fa621e371f3535efd009a1082053
buaabarty <buaabarty@gmail.com>
2023-10-02 18:57:18 +0800

    update

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-api_key = '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' # OpenAI api key
+api_key = '' # OpenAI api key
 poe_tokens = [''] # Poe cookie token, see as https://github.com/ading2210/poe-api
 bard_tokens = ['asdfasdfsdf'] # Bard cookie token see as https://github.com/dsdanielpark/Bard-API
 # if do not use local model cache, set model_dir to ''
